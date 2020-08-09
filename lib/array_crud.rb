46a9e35e8dc69c4645b7fa2547aefaab66229bd9
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-09 00:12:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,10 +1,10 @@
 def create_an_empty_array
   []
-  puts create_an_empty_array
 end
 
 def create_an_array
-  ["e1", "e2", "e3", "e4"] 
+  ["e1", "e2", "e3", "e4"]
+  puts create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
