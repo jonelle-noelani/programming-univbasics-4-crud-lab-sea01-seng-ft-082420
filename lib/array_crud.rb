73e7d7008ef8822495fda5d490a9204a8a4682bd
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-09 00:19:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 p create_an_array
 
 def add_element_to_end_of_array(array, element)
-   create_an_array << (“e5”) 
+   array << “element”
 end
 
 # def add_element_to_start_of_array(array, element)
